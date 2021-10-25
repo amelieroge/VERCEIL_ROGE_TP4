47861cd347981c9c2847f6c8d3427824b3dd580c
ASUS <ASUS@DESKTOP-9DQ73EJ>
2021-10-25 14:35:52 +0200

    descrpition des diagrammes de classe

---
+++ SP4_console_ROGE_EUDES/src/sp4_console_roge_eudes/Jeton.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_roge_eudes;
+
+/*Jeton
+
+-String Couleur
+
++Jeton (String)
++String lireCouleur()
+*/
+public class Jeton {
+    
+}
---
+++ SP4_console_ROGE_EUDES/src/sp4_console_roge_eudes/Joueur.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_roge_eudes;
+/* Joueur
+
+-String Nom
+-String Couleur
+-Jeton [] ListeJetons
+-int nombreDesintegrateurs
+-int nombreJetonsRestants
+
++Joueur (String)
++void affecterCouleur(String)
++boolean recevoirJeton(Jeton)
++void obtenirDesintegrateur()
++boolean utiliserDesintegrateur()
+ */
+public class Joueur {
+   
+}
