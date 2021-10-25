5fdab327dd8ee210f34297124c82304dddd65769
Anais <Anais@LAPTOP-TE767Q1J>
2021-10-25 14:36:11 +0200

    Création classe partie

---
+++ SP4_console_ROGE_EUDES/src/sp4_console_roge_eudes/Partie.java
@@ -0,0 +1,28 @@
+/*Attributs :
+• - ListeJoueurs : tableau des deux joueurs de la partie
+• - joueurCourant : désigne le joueur courant à tout moment de la partie
+• - grilleJeu : désigne la grille de jeu. C’est sans doute l’objet le plus important de
+ce projet. Tout y est fait : ajout de jetons, et vérification de la grille.
+Méthodes :
+• + attribuerCouleursAuxJoueurs(): attribue des couleurs aux joueurs
+• + initialiserPartie() : crée la grille, crée les jetons et les attribue aux joueurs
+correspondants. Place les trous noirs (version 2) et les téléporteurs (version 3).
+• + debuterPartie(): lance la partie
+
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_roge_eudes;
+
+/**
+ *
+ * @author Anais
+ */
+public class Partie {
+    
+    String TabJoueur[] ;
+    String JoueurCourant ;
+    Grille grilleJeu ;
+    
+public Partie ()    
+}
